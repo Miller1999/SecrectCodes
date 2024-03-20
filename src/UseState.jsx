f32176244e7ca54efd5042c142145af78b3886c4
Miller Arias <millerjavierarias@gmail.com>
2024-03-19 22:45:15 -0500

    Estados Imperativos

--- src/UseState.jsx
+++ src/UseState.jsx
@@ -7,6 +7,8 @@ export function UseState({ name }) {
 		value: "",
 		error: false,
 		loading: false,
+		deleted: false,
+		confirmed: false,
 	});
 
 	React.useEffect(() => {
@@ -17,6 +19,7 @@ export function UseState({ name }) {
 						...state,
 						error: false,
 						loading: false,
+						confirmed: true,
 					});
 				} else {
 					setState({
@@ -25,35 +28,83 @@ export function UseState({ name }) {
 						loading: false,
 					});
 				}
-			}, 3000);
+			}, 1000);
 		}
 	}, [state.loading]);
-	return (
-		<div>
-			<h2>Eliminar {name}</h2>
-			<p>Por favor, escribe el codigo de seguridad.</p>
-			{state.error && !state.loading && <p>Error: El codigo es incorrecto</p>}
-			{state.loading && <p>Cargando...</p>}
-			<input
-				placeholder="Codigo de seguridad"
-				value={state.value}
-				onChange={(event) => {
-					setState({
-						...state,
-						value: event.target.value,
-					});
-				}}
-			/>
-			<button
-				onClick={() => {
-					setState({
-						...state,
-						loading: true,
-					});
-				}}
-			>
-				Comprobar
-			</button>
-		</div>
-	);
+
+	if (!state.deleted && !state.confirmed) {
+		return (
+			<div>
+				<h2>Eliminar {name}</h2>
+				<p>Por favor, escribe el codigo de seguridad.</p>
+				{state.error && !state.loading && <p>Error: El codigo es incorrecto</p>}
+				{state.loading && <p>Cargando...</p>}
+				<input
+					placeholder="Codigo de seguridad"
+					value={state.value}
+					onChange={(event) => {
+						setState({
+							...state,
+							value: event.target.value,
+						});
+					}}
+				/>
+				<button
+					onClick={() => {
+						setState({
+							...state,
+							loading: true,
+						});
+					}}
+				>
+					Comprobar
+				</button>
+			</div>
+		);
+	} else if (state.confirmed && !state.deleted) {
+		return (
+			<React.Fragment>
+				<p>Pedimos confirmación, ¿Estas seguro?</p>
+				<button
+					onClick={() => {
+						setState({
+							...state,
+							deleted: true,
+						});
+					}}
+				>
+					Si, eliminar
+				</button>
+				<button
+					onClick={() => {
+						setState({
+							...state,
+							confirmed: false,
+							value: "",
+						});
+					}}
+				>
+					No, volver
+				</button>
+			</React.Fragment>
+		);
+	} else {
+		return (
+			<React.Fragment>
+				<p>Eliminado con exito</p>
+				<button
+					onClick={() => {
+						setState({
+							...state,
+							confirmed: false,
+							deleted: false,
+							value: "",
+						});
+					}}
+				>
+					Recuperar el estado
+				</button>
+			</React.Fragment>
+		);
+	}
 }
